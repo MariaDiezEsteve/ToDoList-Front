2806e262b8553197787484471b8b92f45f5e53c3
JJessa <jessicamosteiro13@gmail.com>
2023-04-18 14:25:56 +0200

    conectado con la base de datos

---
+++ src/dataInfo/dataInfo.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import {ref} from 'vue'
+
+
+async function getData() { 
+    let isError = false;
+    let isLoading = true;
+    let data = ref("")
+
+    try {
+        
+        let response = await axios.get('http://localhost:3000/errands')
+        data.value= await response.data
+
+    }catch (error){
+        isError = true;
+    }
+    isLoading = false;
+
+    return {isError, isLoading, data}
+}
+
+export default {getData}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
+
 <template>
     <div>
         <h1>TO DO LIST</h1>
 
-        <Form />
-        <List />
+        <img src="@/assets/logo.png" alt="" v-if="data.isError">
+        <img src="@/assets/logo.png" alt="" v-if="data.isLoading">
+
+        <div v-if="!data.isError && !data.isLoading">
+
+            <Form />
+            <List :data="data"/>
+
+        </div>
+
+       
 
     </div>
 </template>
@@ -11,6 +21,12 @@
 <script setup>
 import Form from '../components/Form.vue'
 import List from '../components/List.vue'
+import info from '@/dataInfo/dataInfo'
+import { ref, onMounted } from 'vue';
+
+let data = ref(onMounted(async () => {
+    data.value = await info.getData()
+}))
 
 </script>
 
